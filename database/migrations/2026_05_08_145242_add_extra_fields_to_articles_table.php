6037c96a791850e72c1c61c506d4cf5fa4e1a3b9
DnBright <dnbright000@gmail.com>
2026-05-08 21:53:08 +0700

    Add extra fields to articles table

---
+++ database/migrations/2026_05_08_145242_add_extra_fields_to_articles_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('category')->nullable()->after('slug');
+            $table->boolean('is_member_only')->default(false)->after('is_published');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn(['category', 'is_member_only']);
+        });
+    }
+};
