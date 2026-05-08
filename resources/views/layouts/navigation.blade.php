a75cdb9f4fdaf7949d5db7ca3d3fd40681bbafac
DnBright <dnbright000@gmail.com>
2026-05-09 06:25:21 +0700

    Rename Overview to Roadmap in student sidebar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,7 +21,7 @@
         
         <x-sidebar-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-            Overview
+            Roadmap
         </x-sidebar-link>
 
         <x-sidebar-link :href="route('packages.index')" :active="request()->routeIs('packages.index')">
