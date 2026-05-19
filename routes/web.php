98b89bfeb9d1e9659f1c8d8eaffc654da36dd526
DnBright <dnbright000@gmail.com>
2026-05-19 18:39:09 +0700

    chore: clean up temporary routes after successful debug and storage link repair

--- routes/web.php
+++ routes/web.php
@@ -65,46 +65,6 @@ Route::get('/run-seed-saidin', function() {
     }
 });
 
-Route::get('/check-admins', function() {
-    try {
-        $admins = \App\Models\Admin::all(['id', 'name', 'email']);
-        return response()->json($admins);
-    } catch (\Exception $e) {
-        return "Error: " . $e->getMessage();
-    }
-});
-
-Route::get('/run-fix-storage', function () {
-    $link = public_path('storage');
-    $target = storage_path('app/public');
-    
-    try {
-        if (file_exists($link)) {
-            if (is_link($link)) {
-                unlink($link);
-            } else {
-                // If it is a directory, rename or delete it
-                $backup = $link . '_backup_' . time();
-                rename($link, $backup);
-            }
-        }
-        
-        // Recreate target directory if it doesn't exist
-        if (!file_exists($target)) {
-            mkdir($target, 0755, true);
-        }
-        
-        if (app()->make('files')->link($target, $link)) {
-            return "Sukses: Link simbolik storage telah berhasil dibuat dari [$link] ke [$target]!";
-        }
-        
-        // Alternative using artisan command
-        \Illuminate\Support\Facades\Artisan::call('storage:link');
-        return "Sukses: Storage link created via artisan!";
-    } catch (\Exception $e) {
-        return "Error: " . $e->getMessage();
-    }
-});
 
 Route::get('/verification/notice', function () {
     return view('auth.pending');
