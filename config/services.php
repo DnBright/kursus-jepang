f967504d28c799b71304dc06204da8e7aa5d131a
DnBright <dnbright000@gmail.com>
2026-08-14 21:07:59 +0700

    fix: use Google Test Keys to bypass Invalid site key error until real keys are provided

--- config/services.php
+++ config/services.php
@@ -36,8 +36,8 @@ return [
     ],
 
     'recaptcha' => [
-        'site_key' => env('RECAPTCHA_SITE_KEY', 'dummy_site_key_render_force'),
-        'secret_key' => env('RECAPTCHA_SECRET_KEY', 'dummy_secret_key_render_force'),
+        'site_key' => env('RECAPTCHA_SITE_KEY', '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'),
+        'secret_key' => env('RECAPTCHA_SECRET_KEY', '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'),
     ],
 
 ];
