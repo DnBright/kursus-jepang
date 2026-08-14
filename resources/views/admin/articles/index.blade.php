aeadc1ef4c182fa68297c325ec49c2da5ccc3565
DnBright <dnbright000@gmail.com>
2026-08-14 22:02:19 +0700

    fix: allow absolute URLs for article images in admin table

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -37,7 +37,11 @@
                             <td class="p-4">
                                 <div class="flex items-center gap-3">
                                     @if($article->image)
-                                    <img src="{{ Storage::url($article->image) }}" alt="" class="w-10 h-10 rounded-lg object-cover">
+                                        @if(Str::startsWith($article->image, ['http://', 'https://']))
+                                            <img src="{{ $article->image }}" alt="" class="w-10 h-10 rounded-lg object-cover">
+                                        @else
+                                            <img src="{{ Storage::url($article->image) }}" alt="" class="w-10 h-10 rounded-lg object-cover">
+                                        @endif
                                     @else
                                     <div class="w-10 h-10 rounded-lg bg-slate-100 flex items-center justify-center text-slate-400">
                                         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002-2z"></path></svg>
